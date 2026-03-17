e1c02db9de5fa1ec14ba47604fede15b4993c7ba
gigiibandoch <gigiibandoch@gmail.com>
2026-03-17 09:31:59 -0300

    atualizacao

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 const tarefas = ref([
 {
 id: 1,
@@ -47,22 +47,58 @@ function editTarefa(idItem) {
   posicaoAlterada.value = tarefas.value.findIndex(item => item.id == idItem);
   novaTarefa.value = tarefas.value[posicaoAlterada.value].tarefa;
 }
+const filtro = ref('')
+const tarefasFiltradas = computed(() => {
+  if(filtro.value.trim().length > 0) {
+    return tarefas.value.filter( item => item.tarefa.includes(filtro.value));
+  }
+  else {
+    return tarefas.value;
+  }
+})
+function ordenarArray() {
+  tarefas.value.sort((a, b) => a.tarefa.localeCompare(b.tarefa));
+}
 </script>
 
 <template>
 <div class="container">
 <h1>Lista de Tarefas</h1>
-<input type="text" v-model="novaTarefa">
+<input type="text" v-model="novaTarefa" placeholder="Adicionar nova tarefa">
 <button @click="addTarefa">{{ posicaoAlterada == -1 ? "Adicionar" : "Salvar" }}</button>
+
 <ul>
-<li v-for="item in tarefas" :key="item.id">
+<li v-for="item in tarefasFiltradas" :key="item.id"  :class="{ concluida: item.status === 'concluida' }"> >
 {{ item.tarefa }}
 <button @click="delTarefa(item.id)" >X</button>
 <button @click="editTarefa(item.id)" >Editar</button>
 </li>
 </ul>
+<input type="text" placeholder="Filtrar tarefa" v-model="filtro">
+<button @click="ordenarArray" >Ordenar</button>
 </div>
 </template>
 
 <style scoped>
+div h1 {
+  color: rgb(235, 156, 255);
+}
+div {
+  align-items: center;
+}
+div button {
+  background-color: rgb(235, 156, 255);
+  border-radius: 10px;
+  color: rgb(0, 0, 0);
+  padding: 4px 20px;
+  margin: 20px;
+  font-size: 0.8rem;
+}
+div input::placeholder {
+  color: rgb(46, 46, 46);
+}
+.concluida {
+  text-decoration: line-through;
+  color:  rgb(235, 156, 255);
+}
 </style>
